fe9fc6f453384b006f33d0b7381ba578c17bb410
V Pruthvika <vpruthvika2005@gmail.com>
2026-06-08 11:14:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="placement_prediction",
+    version="0.0.0",
+    author="Puth",
+    author_email="vpruthvika2005@gmail.com",
+    packages=find_packages()
+)
